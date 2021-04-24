05261fd867409739a792db754ffafcbd4077a80c
Robert <ibsonnumber1@gmail.com>
2021-04-24 16:57:38 -0400

    Failed Test of convertStringToInt

--- src/main/java/edu/bsu/cs222/RomanNumeralToInt.java
+++ src/main/java/edu/bsu/cs222/RomanNumeralToInt.java
@@ -1,4 +1,7 @@
 package edu.bsu.cs222;
 
 public class RomanNumeralToInt {
+    public int convertStringToInt(String i) {
+        return 0;
+    }
 }
--- src/test/java/edu/bsu/cs222/RomanNumeralToIntTest.java
+++ src/test/java/edu/bsu/cs222/RomanNumeralToIntTest.java
@@ -1,4 +1,13 @@
 package edu.bsu.cs222;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class RomanNumeralToIntTest {
+    RomanNumeralToInt romanNumeralToInt = new RomanNumeralToInt();
+
+    @Test
+    public void testRomanNumeralI(){
+        Assertions.assertEquals(1,romanNumeralToInt.convertStringToInt("I"));
+    }
 }
